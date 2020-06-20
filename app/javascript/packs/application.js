47606088342037423cc51cb7ef4acb82ae6d6e83
Yassine Iha <y.iharkachen@gmail.com>
2020-06-20 22:21:14 +0200

    fr nl split

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,6 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+import "bootstrap";
+import { anime1 } from '../components/anime';
+
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -22,13 +21,6 @@ require("channels")
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
 
-// External imports
-import "bootstrap";
+// External import
 
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
-
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
+anime1();
